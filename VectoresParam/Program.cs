9c265e849131ebe88817d268f62016525c256afd
Renycbz18 <reny.chila@pucese.edu.ec>
2018-12-06 09:21:12 -0500

    primer commit. Vectores como parametros de una funcion

--- VectoresParam/Program.cs
+++ VectoresParam/Program.cs
@@ -8,8 +8,33 @@ namespace VectoresParam
 {
     class Program
     {
+        const int MAX = 10;
         static void Main(string[] args)
         {
+            int[] x = new int[MAX];
+            Leer(x);
+            Imprimir(x);
+            Console.ReadKey();
         }
+
+
+        public static void Leer(int[] x)
+        {
+            for(int i=0; i<MAX; i++)
+            {
+                Console.WriteLine("\nINGRESE EL ELEMENTO {0}", i + 1);
+                x[i] = Int32.Parse(Console.ReadLine());
+
+            }
+        }
+
+        public static void Imprimir (int[] x)
+        {
+            foreach(int num in x)
+            {
+                Console.WriteLine("\nELEMENTO {0}", num);
+            }
+        }
+
     }
 }
